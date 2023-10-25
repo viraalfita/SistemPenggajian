922aad4836962e2f0d953448e96806b26cdc914b
viraalfita <viraalfita1813@gmail.com>
2023-10-25 18:02:59 +0700

    revisi sistem penggajian

--- src/SistemPenggajian.java
+++ src/SistemPenggajian.java
@@ -8,73 +8,78 @@ public class SistemPenggajian {
         String[] password = { "virapass", "diopass", "taufikpass" };
 
         String inputUsername, inputPassword;
-        boolean auth = false;
-        int percobaanMax = 3, userIndex;
+        int hasil = -1; // untuk pengecekan username terdapat dalam array
 
-        for (int i = 0; i < percobaanMax; i++) {
-            System.out.print("Masukkan username : ");
-            inputUsername = scan.nextLine();
-            System.out.print("Masukkan password : ");
-            inputPassword = scan.nextLine();
+        System.out.print("Masukkan username : ");
+        inputUsername = scan.nextLine();
+        System.out.print("Masukkan password : ");
+        inputPassword = scan.nextLine();
 
-            userIndex = -1;
-
-            for (int j = 0; j < username.length; j++) {
-                if (inputUsername.equalsIgnoreCase(username[j]) && inputPassword.equalsIgnoreCase(password[j])) {
-                    userIndex = j;
-                    break;
-                }
-            }
-            if (userIndex != -1) {
-                auth = true;
-                System.out.println("Selamat datang, " + username[userIndex]);
-                System.out.println("");
-
-                int jumlahKaryawan;
-                System.out.println("---------------- Input Karyawan ---------------");
-                System.out.println("");
-                System.out.print("Masukkan jumlah karyawan yang akan di input : ");
-                jumlahKaryawan = scan.nextInt();
-                scan.nextLine();
-
-                String[] namaKaryawan = new String[jumlahKaryawan];
-                String[] alamatKaryawan = new String[jumlahKaryawan];
-                String[] divisiKaryawan = new String[jumlahKaryawan];
-
-                for (int j = 0; j < jumlahKaryawan; j++) {
-                    System.out.println("");
-                    System.out.println("Karyawan ke - " + (j + 1));
-                    System.out.print("Nama    : ");
-                    namaKaryawan[j] = scan.nextLine();
-                    System.out.print("Alamat    : ");
-                    alamatKaryawan[j] = scan.nextLine();
-                    System.out.print("Divisi    : ");
-                    divisiKaryawan[j] = scan.nextLine();
-                }
-
-                System.out.println("");
-                System.out.println("Daftar Karyawan");
-
-                for (int j = 0; j < jumlahKaryawan; j++) {
-                    System.out.println();
-                    System.out.println("Karyawan ke-" + (j + 1));
-                    System.out.println();
-                    System.out.println("Nama Karyawan : " + namaKaryawan[j]);
-                    System.out.println("Alamat Karyawan : " + alamatKaryawan[j]);
-                    System.out.println("Divisi Karyawan : " + divisiKaryawan[j]);
-
-                }
-
+        for (int i = 0; i < username.length; i++) {
+            if (username[i].equalsIgnoreCase(inputUsername)) {
+                hasil = i;
                 break;
-
-            } else {
-                System.out.println("Username atau password salah");
-                System.out.println("Sisa percobaan : " + (percobaanMax - i - 1));
             }
         }
 
-        if (!auth) {
-            System.out.println("Maaf anda gagal login.");
+        if (hasil == -1) {
+            System.out.println("Username tidak ditemukan");
+        } else {
+            System.out.println("Berhasil login");
+            System.out.println();
+            int jumlahKaryawan;
+            System.out.println("---------------- Input Karyawan ---------------");
+            System.out.println("");
+            System.out.print("Masukkan jumlah karyawan yang akan di input : ");
+            jumlahKaryawan = scan.nextInt();
+            scan.nextLine();
+
+            String[] namaKaryawan = new String[jumlahKaryawan];
+            String[] alamatKaryawan = new String[jumlahKaryawan];
+            String[] divisiKaryawan = new String[jumlahKaryawan];
+
+            for (int j = 0; j < jumlahKaryawan; j++) {
+                System.out.println("");
+                System.out.println("Karyawan ke - " + (j + 1));
+                System.out.print("Nama    : ");
+                namaKaryawan[j] = scan.nextLine();
+                System.out.print("Alamat  : ");
+                alamatKaryawan[j] = scan.nextLine();
+                System.out.print("Divisi  : ");
+                divisiKaryawan[j] = scan.nextLine();
+            }
+
+            System.out.println("");
+            System.out.println("Daftar Karyawan");
+
+            for (int j = 0; j < jumlahKaryawan; j++) {
+                System.out.println();
+                System.out.println("Karyawan ke-" + (j + 1));
+                System.out.println();
+                System.out.println("Nama Karyawan : " + namaKaryawan[j]);
+                System.out.println("Alamat Karyawan : " + alamatKaryawan[j]);
+                System.out.println("Divisi Karyawan : " + divisiKaryawan[j]);
+            }
+
+            System.out.println("\n---------------- Search Karyawan ---------------");
+            System.out.print("\nMasukkan nama karyawan yang ingin dicari: ");
+            String cariNama = scan.nextLine();
+
+            boolean ditemukan = false;
+            for (int j = 0; j < jumlahKaryawan; j++) {
+                if (namaKaryawan[j].equalsIgnoreCase(cariNama)) {
+                    ditemukan = true;
+                    System.out.println("\nKaryawan dengan nama " + cariNama + " ditemukan:");
+                    System.out.println("Alamat Karyawan : " + alamatKaryawan[j]);
+                    System.out.println("Divisi Karyawan : " + divisiKaryawan[j]);
+                    break;
+                }
+            }
+
+            if (!ditemukan) {
+                System.out.println("\nKaryawan dengan nama " + cariNama + " tidak ditemukan.");
+            }
+
         }
 
         scan.close();
